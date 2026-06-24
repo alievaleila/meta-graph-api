e4bffb85625b138cea520015631008784460dc37
alievaleila <meliyeva452@gmail.com>
2026-06-24 22:47:45 +0400

    fix: redirect root path to swagger-ui to resolve NoResourceFoundException

---
+++ src/main/java/az/edu/ingressacademy/controller/HomeController.java
@@ -0,0 +1,13 @@
+package az.edu.ingressacademy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String redirectToSwagger() {
+        return "redirect:/swagger-ui.html";
+    }
+}
