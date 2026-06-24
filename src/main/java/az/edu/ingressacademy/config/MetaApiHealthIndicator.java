7bce807e01b23744cb09c8e669c4e547f94369ed
alievaleila <meliyeva452@gmail.com>
2026-06-24 22:36:44 +0400

    fix: explicit PostgreSQL dialect configuration in application.yml

--- src/main/java/az/edu/ingressacademy/config/MetaApiHealthIndicator.java
+++ src/main/java/az/edu/ingressacademy/config/MetaApiHealthIndicator.java
@@ -3,16 +3,34 @@ package az.edu.ingressacademy.config;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 
 @Component
 public class MetaApiHealthIndicator implements HealthIndicator {
+
+    private final RestClient healthRestClient;
+
+    public MetaApiHealthIndicator() {
+
+        this.healthRestClient = RestClient.builder()
+                .baseUrl("https://graph.facebook.com")
+                .build();
+    }
+
     @Override
     public Health health() {
+        try {
 
-        boolean isMetaUp = true;
-        if (isMetaUp) {
-            return Health.up().withDetail("Meta Graph API", "Active and responding").build();
+            healthRestClient.get().uri("/v19.0").retrieve().toBodilessEntity();
+
+            return Health.up()
+                    .withDetail("Meta Graph API", "Server is reachable")
+                    .build();
+        } catch (Exception ex) {
+            return Health.down()
+                    .withDetail("Meta Graph API", "Unreachable")
+                    .withDetail("Reason", ex.getMessage())
+                    .build();
         }
-        return Health.down().withDetail("Meta Graph API", "Unreachable").build();
     }
 }
