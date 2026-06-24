fd5f84fafb6500cc20e6f7cb79d8e62e90bee80b
alievaleila <meliyeva452@gmail.com>
2026-06-24 23:08:11 +0400

    fix(config): configure HTTPS server URLs in Swagger to resolve Failed to Fetch error

--- src/main/java/az/edu/ingressacademy/config/SwaggerConfig.java
+++ src/main/java/az/edu/ingressacademy/config/SwaggerConfig.java
@@ -3,15 +3,29 @@ package az.edu.ingressacademy.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        Server productionServer = new Server();
+        productionServer.setUrl("https://meta-graph-api-production-5b10.up.railway.app");
+        productionServer.setDescription("Production server (Secure)");
+
+
+        Server localServer = new Server();
+        localServer.setUrl("http://localhost:8080");
+        localServer.setDescription("Local development server");
+
         return new OpenAPI()
+                .servers(List.of(productionServer, localServer))
                 .info(new Info()
                         .title("Meta Post Analyzer API")
                         .version("1.0.0")
